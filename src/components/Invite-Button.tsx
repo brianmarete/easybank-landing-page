39e8934cf2a8f60baf12cd00646e2bc060a70d47
Brian Marete <bmarete10@gmail.com>
2024-11-13 14:31:22 +0300

    Fix build errors

--- src/components/Invite-Button.tsx
+++ src/components/Invite-Button.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const InviteButton = () => {
   return (
     <button className="bg-gradient-to-r from-lime-green from-10 to-bright-cyan to-90% text-white px-8 py-3 rounded-full shadow-md hover:bg-white hover:text-soft-red ">
